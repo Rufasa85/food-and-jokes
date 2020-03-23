5003822b29196c00f08d26736e82457a609dd432
Clint Brodar <clintbrodar@gmail.com>
2020-03-23 10:47:08 -0700

    Removed explicit joke category

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("i am here!")
 
-var jokeCategories = ["animal", "career", "celebrity", "dev", "explicit", "fashion", "food", "history", "money", "movie", "music", "political", "religion", "science", "sport", "travel"];
+var jokeCategories = ["animal", "career", "celebrity", "dev", "fashion", "food", "history", "money", "movie", "music", "political", "religion", "science", "sport", "travel"];
 var randomJokeCategory = Math.floor(Math.random() * jokeCategories.length);
 var queryURL = `https://api.chucknorris.io/jokes/random?category=${jokeCategories[randomJokeCategory]}`
 $.ajax({
